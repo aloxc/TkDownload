427e7fb0e4310a4d74fe9a8aba5de6cae563ea89
aloxc <leerohwa@gmail.com>
2023-04-25 22:20:25 +0000

    init

--- TkDownload/Form1.Designer.cs
+++ TkDownload/Form1.Designer.cs
@@ -29,13 +29,49 @@ namespace TkDownload
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(24, 12);
+            this.urlTextBox.Multiline = true;
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(478, 289);
+            this.urlTextBox.TabIndex = 0;
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(638, 37);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(75, 23);
+            this.downloadButton.TabIndex = 1;
+            this.downloadButton.Text = "DownLoad";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.urlTextBox);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TkDownload";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Button downloadButton;
     }
 }
 
--- TkDownload/Form1.cs
+++ TkDownload/Form1.cs
@@ -12,10 +12,21 @@ namespace TkDownload
 {
     public partial class Form1 : Form
     {
+        string[] urls = null;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void downloadButton_Click(object sender, EventArgs e)
+        {
+            string u = this.urlTextBox.Text;
+            if(u == null || u.Length == 0 || u.Trim().Length == 0)
+            {
+                return;
+            }
+            urls = u.Split("\n");
+            MessageBox.Show("" + urls.Length);
+        }
     }
 }
